b69f1ba6129a1b1d0c691c84a15aa0629497842c
chengzh <chengzh@ushareit.com>
2019-05-11 19:30:08 +0800

    完成取景器ViewFinder

--- app/src/main/java/com/little/camerax/MainActivity.kt
+++ app/src/main/java/com/little/camerax/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.little.camerax
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.little.camerax.fragment.CameraFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, CameraFragment()).commit()
     }
 }
